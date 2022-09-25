823373f08e15cf8c9ff1b95a6405335a0341c8ae
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-09-25 18:29:48 -0300

    refactor: Criado os testes da classe Command

--- src/CommandTest.java
+++ src/CommandTest.java
@@ -1,5 +1,6 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class CommandTest {
     public CommandTest() {
@@ -7,21 +8,42 @@ public class CommandTest {
     }
 
     @Test
-    public void testCommandWord() {
+    public void testGetCommandWord() {
+        Command command = new Command(CommandWord.GO, null);
+        assertEquals(CommandWord.GO, command.getCommandWord());
+
     }
 
     @Test
     public void testGetSecondWord() {
+        Command commandHelp = new Command(CommandWord.UNKNOWN, "help");
 
+        assertEquals("help", commandHelp.getSecondWord());
+
+        Command commandNull = new Command(CommandWord.UNKNOWN, null);
+
+        assertEquals(null, commandNull.getSecondWord());
     }
 
     @Test
     public void testIsUnknown() {
+        Command commandUnknown = new Command(CommandWord.UNKNOWN, null);
 
+        assertEquals(true, commandUnknown.isUnknown());
+
+        Command command = new Command(CommandWord.GO, null);
+
+        assertEquals(false, command.isUnknown());
     }
 
     @Test
     public void testHasSecondWord() {
+        Command commandHelp = new Command(CommandWord.UNKNOWN, "help");
 
+        assertEquals(true, commandHelp.hasSecondWord());
+
+        Command commandNull = new Command(CommandWord.UNKNOWN, null);
+
+        assertEquals(false, commandNull.hasSecondWord());
     }
 }
