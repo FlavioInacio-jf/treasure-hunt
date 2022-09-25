7eeac74a6e14211511923792e01249bc9e030d35
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-09-25 19:13:42 -0300

    refactor: Criado as assinatura de metodos da classe de teste Game

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,8 +1,39 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class GameTest {
+  public GameTest() {
+
+  }
+
   @Test
-  public void GameTest() {
+  public void testCreateRooms() {
+
+  }
+
+  @Test
+  public void testPlay() {
+
+  }
+
+  @Test
+  public void testPrintWelcome() {
+
+  }
+
+  @Test
+  public void testPrintHelp() {
+
+  }
+
+  @Test
+  public void testGoRoom() {
+
+  }
+
+  @Test
+  public void testQuit() {
 
   }
 }
