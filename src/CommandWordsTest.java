017722c788ab2771281e43f6925dace51416e0d6
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-09-25 18:49:09 -0300

    refactor: Criado os testes da classe CommandWords

--- src/CommandWordsTest.java
+++ src/CommandWordsTest.java
@@ -1,8 +1,33 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CommandWordsTest {
+
+  public CommandWordsTest() {
+
+  }
+
   @Test
-  public void CommandWordsTest() {
+  public void testGetCommandWorld() {
+
+    CommandWords commandWords = new CommandWords();
+    assertEquals(CommandWord.GO, commandWords.getCommandWord("go"));
+
+    assertEquals(CommandWord.UNKNOWN, commandWords.getCommandWord("other"));
+
+  }
+
+  @Test
+  public void testIsCommand() {
+    CommandWords commandWords = new CommandWords();
+    assertEquals(true, commandWords.isCommand("help"));
+    assertEquals(false, commandWords.isCommand("other"));
+
+  }
+
+  @Test
+  public void testShowAll() {
 
   }
 }
